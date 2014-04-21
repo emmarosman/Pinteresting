ce1b670e3dea1c1b6d9a51c1b8f98f4753c8296a
Emma Rosman <rosman.emma@gmail.com>
2014-04-21 13:48:09 +0800

    Add name to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,9 @@
 class Pin < ActiveRecord::Base
     Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.8.9-Q16"
-     belongs_to :user
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+    belongs_to :user
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+  	validates :image, presence: true
+  	validates :description, presence: true
+
 end
