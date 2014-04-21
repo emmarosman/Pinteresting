5d94ddaadea6674f4961ce6711a7ddfec91a55ad
Emma Rosman <rosman.emma@gmail.com>
2014-04-21 09:57:18 +0800

    Added paperclip options in pin.rb to see pictures in local host

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,5 @@
 class Pin < ActiveRecord::Base
-	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+    Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.8.9-Q16"
+     belongs_to :user
+     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
